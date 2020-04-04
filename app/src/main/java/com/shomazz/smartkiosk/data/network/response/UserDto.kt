1088d97ecd5acbc846c2c3c8d7487852ecad36ee
shamil10031 <shamil10031@mail.ru>
2020-04-04 18:58:28 +0300

    Fix user info response

--- app/src/main/java/com/shomazz/smartkiosk/data/network/response/UserDto.kt
+++ app/src/main/java/com/shomazz/smartkiosk/data/network/response/UserDto.kt
@@ -3,7 +3,7 @@ package com.shomazz.smartkiosk.data.network.response
 import com.google.gson.annotations.SerializedName
 
 data class UserDto (
-    @SerializedName("name") val name: String,
-    @SerializedName("organization") val organization: String,
-    @SerializedName("payload") val payload: String
+    @SerializedName("userName") val name: String,
+    @SerializedName("userOrganization") val organization: String,
+    @SerializedName("userPayload") val payload: String
 )
